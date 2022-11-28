91a29aa4a14c608940938d63c1e893fe36b170f5
Combatd <combatdstar@gmail.com>
2022-11-28 14:25:14 -0800

    Define #findTwoSum

--- twoSum.js
+++ twoSum.js
@@ -25,3 +25,23 @@
 numberToFind = target - nums[p1]
 10 = 11 - 1 when starting at the beginning
 */
+
+function findTwoSum(nums, target) {
+
+  for (let i = 0; i < nums.length; i++) {
+    let numberToFind = target - nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (numberToFind === nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+  return null; // if there is empty or 1 element, it passes through the for loop and reaches null anyway
+}
+
+
+console.log(findTwoSum([1, 3, 7, 9, 2], 11))
+console.log(findTwoSum([1, 3, 7, 9, 2], 25))
+console.log(findTwoSum([], 1))
+console.log(findTwoSum([5], 5))
+console.log(findTwoSum([1, 6], 7))
